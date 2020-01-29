271611f84451dd8f6765563d5887b5530f4dce3a
lihongxun945 <lihongxun945@gmail.com>
2020-01-29 16:35:31 +0800

    lesson13

---
+++ lesson13/main.py
@@ -0,0 +1,64 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import datasets
+
+# x: [60k, 28, 28]
+# y: [60k]
+
+(x, y), _ = datasets.mnist.load_data()
+
+x = tf.convert_to_tensor(x, dtype=tf.float32) / 255
+y = tf.convert_to_tensor(y, dtype=tf.int32)
+
+print(x.shape, y.shape, x.dtype, y.dtype)
+print(tf.reduce_min(x), tf.reduce_min(y))
+
+train_db = tf.data.Dataset.from_tensor_slices(((x, y))).batch(128)
+
+
+# [b, 784] => [b, 256], => [b, 128] => [b, 10]
+
+w1 = tf.Variable(tf.random.truncated_normal([784, 256], stddev=0.1))
+b1 = tf.Variable(tf.zeros([256]))
+w2 = tf.Variable(tf.random.truncated_normal([256, 128], stddev=0.1))
+b2 = tf.Variable(tf.zeros([128]))
+w3 = tf.Variable(tf.random.truncated_normal([128, 10], stddev=0.1))
+b3 = tf.Variable(tf.zeros([10]))
+
+for step, (x, y) in enumerate(train_db):
+    # x:[128, 28, 28]
+    # y:[128]
+
+    # [b, 28, 28] => [b, 28*28]
+    x = tf.reshape(x, [-1, 28*28])
+
+    lr = 0.001
+
+    with tf.GradientTape() as tape:
+        # x: [b, 28*28]
+        # h1 = x@w1 + b1
+        # [b, 784] @[784, 256] + [256] => [b ,256]
+        h1 = x@w1 + b1
+        h1 = tf.nn.relu(h1)
+        h2 = h1@w2 + b2
+        h2 = tf.nn.relu(h2)
+        out = h2@w3 + b3
+
+        # compute loss
+        # out: [b, 10]
+        # y: [b] => [b, 10]
+        y_onehot = tf.one_hot(y, depth=10) # one hot 是把0~9的值变成一个向量表示，比如 1 变成 [0, 1, 0, 0, 0, 0, 0, 0, 0, 0]
+
+        # mse = mean(sum(y-out)^2)
+        loss = tf.reduce_mean(tf.square(y_onehot - out))
+
+    grads = tape.gradient(loss, [w1, b1, w2, b2, w3, b3])
+    w1.assign_sub(lr * grads[0])
+    b1.assign_sub(lr * grads[1])
+    w2.assign_sub(lr * grads[2])
+    b2.assign_sub(lr * grads[3])
+    w3.assign_sub(lr * grads[4])
+    b3.assign_sub(lr * grads[5])
+
+    if step % 100 == 0:
+        print('loss:', loss)
